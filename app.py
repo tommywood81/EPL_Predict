71c393656f73cd1701c0a83a18e4aed4072237f3
thomaswood81 <103301159@student.swin.edu.au>
2025-02-25 01:32:34 +1100

    Commit message for branch-one changes

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def predict_route():
     return render_template('index.html', elo_data=elo_data, prediction=prediction_dict)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
